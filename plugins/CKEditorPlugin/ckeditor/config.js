b047bb32fe4bc102469c7016fa38a0e9b5c8bf79
John Crisp <jcrisp@safeandsoundit.co.uk>
2016-02-04 17:45:40 +0100

    Adding ckeditor 4.5.7 optimised

--- plugins/CKEditorPlugin/ckeditor/config.js
+++ plugins/CKEditorPlugin/ckeditor/config.js
@@ -1,21 +1,10 @@
-﻿/*
- * Generated from the config editor is samples
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
+
 CKEDITOR.editorConfig = function( config ) {
-	config.toolbar = [
-		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] , items: [ 'Source' ] },
-		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] , items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
-		{ name: 'editing', groups: [ 'Find', 'Replace', 'selection', 'spellchecker' ] , items: [ 'Scayt' ] },
-		{ name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
-		{ name: 'insert', items: [ 'Image', 'Table', 'HorizontalRule', 'SpecialChar' ] },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] , items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
-		{ name: 'align', items: [ 'JustifyLeft', 'JustifyCenter', 'JustifyBlock', 'JustifyRight' ] },
-		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] , items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote' ] },
-		'/',
-		{ name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize', 'TextColor', 'BGColor' ] },
-		{ name: 'tools', items: [ 'Maximize' ] },
-		{ name: 'others', items: [ '-' ] },
-		{ name: 'about', items: [ 'About' ] }
-	];
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
 };
